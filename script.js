a6865cf234840247a9d585217782a638c15280b8
Garrett Steed <steed.garrett@gmail.com>
2020-10-29 13:55:17 -0600

    got the api to fecth correctly

--- script.js
+++ script.js
@@ -8,19 +8,50 @@ $(".clickSearch").on("click", function(e) {
     e.preventDefault()
     var citySearch = $("#textInput").val();
     console.log(citySearch);
-    callApi();
-})
 
-function callApi(citySearch) {
-    var fetchApi = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=637169d507746a6cc76b508c540669a2&units=imperial"
-    fetch(fetchApi)
-    .then(function (response) {
-        console.log(response)
-    })
-}
+    //clear input box
+    $("#textInput").val("")
+    
+    callApi(citySearch);
+});
 
 
+ function callApi(citySearch) {
+    var key = '637169d507746a6cc76b508c540669a2';
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        citySearch +
+        '&appid=' +
+        key + 
+        '&units=imperial'
+    )
+      .then(function (response) {
+        return response.json();
+      }) // Convert data to json
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function () {
+        // catch any errors
+      });
+  }
 
+
+// var title =$("<h3>").addClass("card-title").text(data.name + "")
+
+
+
+
+
+
+// $(".lastCity").on("click", "li", function() {
+//     callApi($(this).text());
+// })
+
+// function makeRow(text) {
+//     var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+//     $(".lastCity").append(li);
+// }
 
 
 
