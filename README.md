9178226da091dc5d6265fbb2452b1742280ab978
Garrett Steed <steed.garrett@gmail.com>
2020-11-01 21:17:50 -0700

    uploading 5 day and other css changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # weather-dashboard
+
+This project will show a weather dashboard that uses api calls to get the local weather and a 5 days forecast. You can input a city name into the search bar and it will tap into the api and present the data.
+
+The data should come back with temp, humidity, windspeed and more. It will also have icons attached to make it more uer and visibly friendly.
+
+I will also store the past searches in local storage so you can refer back to them.
+
+Git Hub page:
+
+https://github.com/gsteed1677/weather-dashboard
+
+You may notice in the code that I did the fetch in two different ways. i had issues with the AJAX when starting the project but learned more with a tutor and in class and built the rest of it out with that method.
--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //document to pull in all js before all loads
 
 $(document).ready(function() {
+var key = '637169d507746a6cc76b508c540669a2';
+var forecastDiv = $("#weatherForecast");
+
+if (localStorage.getItem("citySearch")) {
+    citiesArray = JSON.parse(localStorage.getItem("citySearch"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
+
 
 //created function for on click button event
 $(".clickSearch").on("click", function(e) {
@@ -8,16 +18,19 @@ $(".clickSearch").on("click", function(e) {
     e.preventDefault()
     var citySearch = $("#textInput").val();
     console.log(citySearch);
-
     //clear input box
     $("#textInput").val("")
     
     callApi(citySearch);
-});
+    fiveDayForecast(citySearch);
+})
 
 
- function callApi(citySearch) {
-    var key = '637169d507746a6cc76b508c540669a2';
+// //Local storage 
+
+
+//call api and fetch data to be used
+function callApi(citySearch) {
     fetch(
       'https://api.openweathermap.org/data/2.5/weather?q=' +
         citySearch +
@@ -30,38 +43,68 @@ $(".clickSearch").on("click", function(e) {
       }) // Convert data to json
       .then(function (data) {
         console.log(data);
+
+        //clear container for new search
+        $("#today").empty();
+
+        //var for html creation and attributes
+        var title = $("<h3>").addClass("card-title").text(data.name);
+        var card = $("<div>").addClass("card");
+        var wind = $("<p>").addClass("card-text").text("Wind Speed " + data.wind.speed + " MPH");
+        var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+        var temp = $("<p>").addClass("card-text").text("Temperature " + data.main.temp + " F");
+        var cardBody = $("<div>").addClass("card-body");
+        var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
+        title.append(img);
+        cardBody.append(title, temp, humid, wind);
+        card.append(cardBody);
+        $("#today").append(card);
+
+        //You could call a function that adds your search term to the array of search history
       })
       .catch(function () {
         // catch any errors
       });
-  }
+
+    }
+
+
+//new function for five day forecast - using AJAX from last weeks class
+function fiveDayForecast(citySearch) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&units=imperial&APPID=${key}`;
+
+    $.get(queryURL).then(function(response){
+        let forecastInfo = response.list;
+        forecastDiv.empty();
+        $.each(forecastInfo, function(i) {
+            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+
+            forecastDiv.append(`
+            <div class="col-md">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${forecastInfo[i].main.temp} F</p>
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    })
+};
 
 
 
+  // local storage and dom function
+  // 1. need add search history
+  // 2. crea
 
 
-
-
-// $("#today").empty();
-
-// var title = $("<h3>").createClass("card-title").text(data.name);
-// var card = $("<div>").addClass("card");
-// var wind = $("<p>").addClass("card-text").text("Wind Speed " + data.wind.speed + "MPH");
-// var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
-// var temp = $("<p>").addClass("card-text").text("Temperature " + data.main.temp + "F");
-// var cardBody = $("<div>").addClass("card-body");
-// var img = $("<img>").attr("src", "http://openweathermap.org/img/w" + data.weather[0].icon + ".png");
-
-// title.append(img);
-// cardBody.append(title, temp, humid, wind);
-// card.append(cardBody);
-// $("#today").append(card);
-
-
-
-
-
-
-
-
-})
+  })
